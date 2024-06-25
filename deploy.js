53216a048f6299073a2d13c4a6d93f88358eb3e7
nishikino25 <cute01jenny@gmail.com>
2024-06-25 11:34:46 +0800

    0625 Update main function

--- deploy.js
+++ deploy.js
@@ -1,4 +1,5 @@
 const { REST, Routes } = require('discord.js');
+const { DISCORD_TOKEN,CLIENT_ID,GUILD_ID } = require('./config.json');
 
 const commands = [
   {
@@ -7,14 +8,20 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken('your_discord_token');
+// const rest = new REST({ version: '10' }).setToken('your_discord_token');
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+var CLIENT_ID = process.env.CLIENT_ID;
+var GUILD_ID = process.env.GUILD_ID;
+
+console.log(process.env.DISCORD_TOKEN, CLIENT_ID, GUILD_ID);
+
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    var CLIENT_ID="your_discord_clientid";
-    var GUILD_ID="your_discord_guild id";
+    // var CLIENT_ID="your_discord_clientid";
+    // var GUILD_ID="your_discord_guild id";
 
     // await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-// const { Client, GatewayIntentBits } = require('discord.js');
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js'); //顯示正在做...模式
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js'); //顯示正在做...模式
+const { DISCORD_TOKEN } = require('./config.json');
 
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-    client.user.setStatus('online'); //設置狀態為在線
+// When the client is ready, run this code (only once).
+client.once(Events.ClientReady, readyClient => {
+  client.user.setStatus('online'); //設置狀態為在線
     // client.user.setStatus('idle'); //設置狀態為閒置
     // client.user.setStatus('dnd'); //設置狀態為請勿打擾
     // client.user.setStatus('invisible'); //設置狀態為離線（隱形）
 
-    client.user.setActivity('你的電腦', { type: ActivityType.Watching }); //將機器人的行為設置為正在玩iTHome
+  client.user.setActivity('你的電腦', { type: ActivityType.Watching }); //將機器人的行為設置為正在玩iTHome
     // client.user.setActivity('前綴後面的文字', { type: ActivityType.Watching }); //將機器人的行為設置為正在看
     // client.user.setActivity('前綴後面的文字', { type: ActivityType.Listening }); //將機器人的行為設置為正在聽
     // client.user.setActivity('前綴後面的文字', { type: ActivityType.Streaming }); //將機器人的行為設置為正在直播
     // client.user.setActivity('前綴後面的文字', { type: ActivityType.Playing }); //將機器人的行為設置為正在玩
     // client.user.setActivity('前綴後面的文字', { type: ActivityType.Competing }); //將機器人的行為設置為競爭
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -26,5 +29,8 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-// client.login('token');   
-client.login('your_discord_token');
+// Log in to Discord with your client's token
+// client.login('your_discord_token');
+client.login(DISCORD_TOKEN);
+console.log('Starting Discord bot...');
+
